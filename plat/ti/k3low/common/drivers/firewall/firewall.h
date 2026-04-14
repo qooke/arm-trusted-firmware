ad6ffb14d1ad880bd1e849e138c9ea8f3623d45f
T Pratham <t-pratham@ti.com>
2026-04-14 15:24:58 +0530

    feat(k3low): optimize firewall configuration to minimize boot time
    Currently, ROM configured firewalls are removed during SOC init for
    AM62L by iterating over all regions of the firewalls and then taking
    action if they are active. In this process, we end up making an
    unnecessarily large number of TISCI calls, which negatively impacts the
    boot time.
    
    Instead, pre-determine the list of firewall IDs and respective regions
    configured by ROM, and add a static table to only re-configure those
    regions. This greatly reduces the number of TISCI calls we are making,
    and hence minimizes boot time impact.
    
    While re-configuring, set a permissive background firewall enabling
    access to everyone. This is equivalent to keeping them disabled.
    However, when going into low-power modes, TIFS doesn't save context for
    disabled firewalls. It only concerns itself with enabled firewalls.
    Hence, if we disable them instead, ROM comes up again in the resume
    sequence to make the same configurations. By keeping the firewalls
    enabled, TIFS will save their context during suspend, and handle their
    restoration during resume, thereby undoing anything ROM had done in
    resume. This is neater and faster than TF-A itself handling the firewall
    configuration again in resume.
    
    Change-Id: I9ba7fa112a8d10f08f887fbb5a13e33177d60a80
    Signed-off-by: T Pratham <t-pratham@ti.com>

--- plat/ti/k3low/common/drivers/firewall/firewall.h
+++ plat/ti/k3low/common/drivers/firewall/firewall.h
@@ -16,28 +16,33 @@
 #define FW_BACKGROUND_BIT	BIT(8U)
 
 /* Firewall IDs */
+#define DDR_FWL_ID		1U
 #define OSPI_FWL_ID		97U
 #define ADC_MCASP_FWL_ID	160U
 
-/* Number of firewall regions */
-#define OSPI_FWL_NUM_REGIONS		8U
-#define ADC_MCASP_FWL_NUM_REGIONS	16U
+/* Firewall Regions to configure */
+#define DDR_BG_FWL_REGION	0U
+#define OSPI_FWL_REGION		0U
+#define ADC_MCASP_FWL_REGION	0U
 
-enum k3_fwl_region_type {
-	K3_FWL_REGION_FOREGROUND = 0,
-	K3_FWL_REGION_BACKGROUND = FW_BACKGROUND_BIT,
-};
+/* Firewall Control Register Values */
+#define FWL_CTRL_EN		0xA
+#define FWL_CTRL_EN_BG		(FWL_CTRL_EN | FW_BACKGROUND_BIT)
+
+/* Firewall permission values */
+#define FWL_PERM_ALL_RW		0xC3BBBB /* RW access to ALL hosts */
 
 struct fwl_data {
 	uint16_t fwl_id;
-	uint8_t num_regions;
+	uint8_t region;
 };
 
 /*
- * Updates firewall configurations by disabling ROM-configured firewalls.
- * This function should be called during boot initialization and after
- * resume from low power mode to ensure firewall regions that were
- * configured by ROM for the boot phase are properly disabled.
+ * ROM configures some firewalls initially for its use. Re-configure these
+ * firewalls with a permissive configuration so that other users can access
+ * these regions after boot.
+ *
+ * Additionally, configure a permissive background firewall over the DDR
  */
 void update_fwl_configs(void);
 
