d014579d3e96f99a885a2ee0702bd582d1f5d3d8
Ryan Everett <ryan.everett@arm.com>
2026-04-15 10:51:24 +0000

    fix(tc): don't use non-secure UART
    Align FVP and FPGA UART usage
    
    Change-Id: If574683c53dbc245b2d4978998d12cbd640c02b7
    Signed-off-by: Jagdish Gediya <jagdish.gediya@arm.com>
    Signed-off-by: Ryan Everett <ryan.everett@arm.com>

--- plat/arm/board/tc/include/platform_def.h
+++ plat/arm/board/tc/include/platform_def.h
@@ -500,11 +500,6 @@
 #define PLAT_ARM_FIP_OFFSET_IN_GPT		0x6000
 #endif /* ARM_GPT_SUPPORT */
 
-/* UART related constants */
-
-#define TC_UART0			0x2a400000
-#define TC_UART1			0x2a410000
-
 /*
  * TODO: if any more undefs are needed, it's better to consider dropping the
  * board_css_def.h include above
@@ -519,20 +514,16 @@
 #undef  ARM_CONSOLE_BAUDRATE
 #define ARM_CONSOLE_BAUDRATE		38400
 
+#define TC_UART1			0x2a410000
+
 #if TARGET_PLATFORM == 3
 #define TC_UARTCLK			3750000
 #elif TARGET_PLATFORM == 4
 #define TC_UARTCLK			4000000
 #endif /* TARGET_PLATFORM == 3 */
 
-
-#if TARGET_FLAVOUR_FVP
 #define PLAT_ARM_BOOT_UART_BASE		TC_UART1
-#else /* TARGET_FLAVOUR_FPGA */
-#define PLAT_ARM_BOOT_UART_BASE		TC_UART0
-#endif /* TARGET_FLAVOUR_FPGA */
-
-#define PLAT_ARM_RUN_UART_BASE		TC_UART0
+#define PLAT_ARM_RUN_UART_BASE		PLAT_ARM_BOOT_UART_BASE
 #define PLAT_ARM_CRASH_UART_BASE	PLAT_ARM_RUN_UART_BASE
 
 #define PLAT_ARM_BOOT_UART_CLK_IN_HZ	TC_UARTCLK
