qooke/arm-trusted-firmware

commit 27e7222106021042451660104f58967a86387f6a

feat(versal2): validate non-secure entry addr
Added validate_ns_entrypoint to get reserved-memory entries from
the Flattened Device Tree (FDT). Identifies secure and non-secure
memory regions and checks if entry point lies within a valid
non-secure region.

Change-Id: Iff998fe855f5de8fbd96f0d7d4b0d7c33c904d34
Signed-off-by: Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>

plat/amd/versal2/plat_psci_pm.c
@@ -14,6 +14,7 @@
 #include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
 #include <plat_arm.h>
+#include <plat_fdt.h>
 
 #include "def.h"
 #include <ipi.h>
@@ -186,6 +187,34 @@ err:
 	return;
 }
 
+static int32_t versal2_validate_ns_entrypoint(uint64_t ns_entrypoint)
+{
+	int32_t ret = PSCI_E_SUCCESS;
+	struct reserve_mem_range *rmr;
+	uint32_t index = 0, counter = 0;
+
+	rmr = get_reserved_entries_fdt(&counter);
+
+	VERBOSE("Validate ns_entry point %lx\n", ns_entrypoint);
+
+	if (counter != 0) {
+		while (index < counter) {
+			if ((ns_entrypoint >= rmr[index].base) &&
+				       (ns_entrypoint <= rmr[index].size)) {
+				ret = PSCI_E_INVALID_ADDRESS;
+				break;
+			}
+			index++;
+		}
+	} else {
+		if ((ns_entrypoint >= BL31_BASE) && (ns_entrypoint <= BL31_LIMIT)) {
+			ret = PSCI_E_INVALID_ADDRESS;
+		}
+	}
+
+	return ret;
+}
+
 static void versal2_pwr_domain_on_finish(const psci_power_state_t *target_state)
 {
 	(void)target_state;
@@ -312,6 +341,7 @@ static const struct plat_psci_ops versal2_nopmc_psci_ops = {
 	.pwr_domain_suspend_finish      = versal2_pwr_domain_suspend_finish,
 	.system_off                     = versal2_system_off,
 	.system_reset                   = versal2_system_reset,
+	.validate_ns_entrypoint		= versal2_validate_ns_entrypoint,
 	.validate_power_state           = versal2_validate_power_state,
 	.get_sys_suspend_power_state    = versal2_get_sys_suspend_power_state,
 };

commit 8b90e801cc9284cd73cf5391332f4de6673f01a8

fix(versal2): correct NS entry point check for reserved regions
The NS entrypoint validation logic incorrectly compared the entrypoint
address against size rather than the end of the reserved region
(base + size). As a result, entrypoints within reserved‑memory regions
could incorrectly pass validation.
Correct the check to treat each reserved region as [base, base + size).

Fixes: 27e722210602 ("feat(versal2): validate non-secure entry addr")
Change-Id: Ifc19f31295c6fb034862e3bca78e1d8659eb86a2
Signed-off-by: Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>

plat/amd/versal2/plat_psci_pm.c
@@ -204,7 +204,7 @@ static int32_t versal2_validate_ns_entrypoint(uint64_t ns_entrypoint)
 	if (counter != 0U) {
 		while (index < counter) {
 			if ((ns_entrypoint >= rmr[index].base) &&
-				       (ns_entrypoint <= rmr[index].size)) {
+			    (ns_entrypoint < (rmr[index].base + rmr[index].size))) {
 				ret = PSCI_E_INVALID_ADDRESS;
 				break;
 			}